0688bdb32eb7ad61fa72533539cad49acd0862e3
Lord448 <pedroeroca@outlook.com>
2023-12-01 23:16:36 -0600

    Long duration Parsed Loop ISR

--- Practica3/UART/software/UART_LCD/obj/default/hello_world_small.d
+++ Practica3/UART/software/UART_LCD/obj/default/hello_world_small.d
@@ -3,6 +3,7 @@ obj/default/hello_world_small.o: hello_world_small.c \
  ../UART_LCD_bsp//HAL/inc/sys/alt_irq.h ../UART_LCD_bsp//HAL/inc/nios2.h \
  ../UART_LCD_bsp//HAL/inc/alt_types.h ../UART_LCD_bsp/system.h \
  ../UART_LCD_bsp/linker.h ../UART_LCD_bsp//HAL/inc/sys/alt_sys_init.h \
+ ../UART_LCD_bsp//drivers/inc/altera_avalon_pio_regs.h \
  ../UART_LCD_bsp//HAL/inc/io.h ../UART_LCD_bsp//HAL/inc/alt_types.h
 ../UART_LCD_bsp//HAL/inc/sys/alt_stdio.h:
 ../UART_LCD_bsp//HAL/inc/sys/alt_irq.h:
@@ -11,5 +12,6 @@ obj/default/hello_world_small.o: hello_world_small.c \
 ../UART_LCD_bsp/system.h:
 ../UART_LCD_bsp/linker.h:
 ../UART_LCD_bsp//HAL/inc/sys/alt_sys_init.h:
+../UART_LCD_bsp//drivers/inc/altera_avalon_pio_regs.h:
 ../UART_LCD_bsp//HAL/inc/io.h:
 ../UART_LCD_bsp//HAL/inc/alt_types.h:
